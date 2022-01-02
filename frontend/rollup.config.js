7447e8f29384226b3e98e3e56bd7d43637ef5f77
Mark Nash <abacefabmicroblast@hotmail.com>
2022-01-01 21:18:24 -0500

    Added s to description

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -48,7 +48,7 @@ export default {
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
 
-		// Copy the windows 98 style theme css file to the public directory
+		// Copy the windows 98 style theme css files to the public directory
 		copy({
 			targets: [{ 
                 src: 'node_modules/98.css/dist/**/*', 
