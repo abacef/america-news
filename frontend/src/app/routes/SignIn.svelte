783cb13825668b0c1e74c879ab5d1b40882c1650
Mark Nash <abacefabmicroblast@hotmail.com>
2022-01-02 23:40:25 -0500

    Give into pouplism and only add line when necessary

--- frontend/src/app/routes/SignIn.svelte
+++ frontend/src/app/routes/SignIn.svelte
@@ -1,6 +1,6 @@
 <script>
-    $: errorMessageUsername = username === "bad" ? "❌ username is bad" : null;
-    $: errorMessagePassword = password === "bad" ? "❌ password is bad" : null;
+    $: errorMessageUsername = username === "bad" ? "❌ username is bad" : "";
+    $: errorMessagePassword = password === "bad" ? "❌ password is bad" : "";
 
 
     let username = "";
@@ -16,11 +16,12 @@
     <div class="window-body">
         <div class="field-row-stacked" style="width: 200px">
             <input id="username-field" type="text" placeholder="Username" bind:value={username}/>
-            <label for="username-field">{errorMessageUsername || "…"}</label>
+            <label for="username-field">{errorMessageUsername}</label>
         </div>
+        <br>
         <div class="field-row-stacked" style="width: 200px">
             <input id="password-field" type="text" placeholder="Password" bind:value={password}/>
-            <label for="password-field">{errorMessagePassword || "…"}</label>
+            <label for="password-field">{errorMessagePassword}</label>
         </div>
         <table width="100%" border="0" cellspacing="0" cellpadding="0">
             <tr>
